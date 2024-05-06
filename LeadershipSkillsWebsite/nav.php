e78429cdbeeaa905d896ce3dc817447d7a5b1174
JDR-SMT <30058666@tafe.wa.edu.au>
2024-05-06 19:39:42 +0800

    Layout

---
+++ LeadershipSkillsWebsite/nav.php
@@ -0,0 +1,30 @@
+<header class="navbar navbar-expand-sm">
+    <div class="container-fluid">
+        <div class="navbar-brand">
+            <img id="logo" src="Icons/logo.png" style="width: 40px;height: 40px;">
+            <div id="title">LEADERSHIP SKILLS</div>
+        </div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse collapse" id="collapseNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.html">
+                        <img src="Icons/house.png" style="width: 35px;height: 35px;">
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="questions.html">
+                        <img src="Icons/questions.png" style="width: 30px;height: 30px;">
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.html">
+                        <img src="Icons/contact.png" style="width: 30px;height: 30px;">
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</header>
